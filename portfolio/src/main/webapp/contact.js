fe95c0049bdd76d6f018cf457c354fc6c5330d13
Raluca Tudor <tudorr30@gmail.com>
2020-07-15 19:28:29 +0000

    Add text, Font Awesome icons & link to socials

--- portfolio/src/main/webapp/contact.js
+++ portfolio/src/main/webapp/contact.js
@@ -1,5 +1,8 @@
 function getContact() {
     wrapper.innerHTML = `
-        <p>Contact section under construction!</p>
+        <h1>Contact Me</h1>
+        <section style="text-align:center;">
+            <a class="social-button" href="https://www.linkedin.com/in/raluca-tudor">LinkedIn Account <i class="fa fa-linkedin" aria-hidden="true" style="font-size:35px; color: #0e76a8;"></i></a>
+        </section>
     `;
 }
--- portfolio/src/main/webapp/projects.js
+++ portfolio/src/main/webapp/projects.js
@@ -1,5 +1,17 @@
 function getProjects() {
     wrapper.innerHTML = `
-        <p>Projects section under construction!</p>
+        <p>Here are two of my latest projects:</p>
+        <h2>Doodle Jump Game</h2>
+        <h3>[C++/SFML] Object Oriented Programming Project</h3>
+        <p>The project can be found <a href="https://github.com/ralucatudor/doodle-jump">here</a>.</p>
+
+        <h2>Zenodotus - Home library management</h2>
+        <h3>Single-page web application</h3>
+        <p>HTML/CSS, JavaScript and Node.js</p>
+        <p>The project can be found <a href="https://github.com/ralucatudor/Zenodotus">here</a>.</p>
+
+        <section style="text-align:center;">
+            <a class="social-button" href="https://github.com/ralucatudor">GitHub Account <i class="fa fa-github" aria-hidden="true" style="font-size:35px;"></i></a>
+        </section>
     `;
 }
